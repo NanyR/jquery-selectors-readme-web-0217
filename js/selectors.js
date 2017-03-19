d3550f90c3c3dee405532fcdf95c9558fb3d8f63
nanyr <nanyramirez0@gmail.com>
2017-03-19 12:57:38 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector(){
+  let paragraphs=$('p');
+  return paragraphs;
+}
+
+function lastImageSelector(){
+  let last= $('img:last');
+  return last;
+}
+function ninjaBabySelector(){
+  let baby=$('#baby-ninja');
+  return baby;
+}
+function divSelector(){
+  let divs=$('.pics');
+  return divs;
+}
+function firstListItem(){
+  let first=$('#pic-list li:first-child');
+  return first
+}
